2d8ded7295e666521c8f4f0c8174075cd0fb5829
Wade Phillips <phillips.acupuncture@gmail.com>
2021-06-14 11:53:35 -0800

    Testing option flags

--- src/Commands/ForumConverterCommand.php
+++ src/Commands/ForumConverterCommand.php
@@ -3,15 +3,44 @@
 namespace wadelphillips\ForumConverter\Commands;
 
 use Illuminate\Console\Command;
+use function collect;
 
 class ForumConverterCommand extends Command
 {
-    public $signature = 'ee-forum:migrate';
+    public $signature = 'ee-forum:migrate
+                        {--a|all : Migrate all categories, forums, topics, and comments at one time }
+                        {--C|categories : Migrate the categories only}
+                        {--f|forums : Migrate the forums only}
+                        {--t|topics : Migrate the topics only}
+                        {--c|comments : Migrate the comments only}';
 
-    public $description = 'Migrates ExpressionEngine 2 forum components into a Wordpress structure sutible for use with BBpress or Buddy Boss';
+    public $description = 'Migrates ExpressionEngine 2 forum components into a Wordpress structure suitable for use with BBPress or Buddy Boss';
 
     public function handle()
     {
+        $arguments = $this->arguments();
+        $options = $this->options();
+
+        if ($options['all']) {
+            $this->info('Migrating All Forum Components');
+        }
+
+        if ($options['categories']) {
+            $this->info('Migrating Forum Categories');
+        }
+
+        if ($options['forums']) {
+            $this->info('Migrating Forums');
+        }
+
+        if ($options['topics']) {
+            $this->info('Migrating Forum Topics');
+        }
+
+        if ($options['comments']) {
+            $this->info('Migrating Forum Comments');
+        }
+
         $this->comment('All done');
     }
 }
--- tests/Commands/ForumConverterCommandTest.php
+++ tests/Commands/ForumConverterCommandTest.php
@@ -11,11 +11,82 @@ class ForumConverterCommandTest extends TestCase
     /** @test */
     public function the_forum_converter_command_works()
     {
-        //arrange
-
-        //act
         $this->artisan('ee-forum:migrate')->assertExitCode(0);
-        //assert
+    }
+
+    /** @test */
+    public function it_accepts_a_categories_flag()
+    {
+
+        $this->artisan('ee-forum:migrate --categories')
+            ->expectsOutput('Migrating Forum Categories')
+            ->assertExitCode(0);
+
+
+        $this->artisan('ee-forum:migrate -C')
+            ->expectsOutput('Migrating Forum Categories')
+            ->assertExitCode(0);
+
+    }
+
+    /** @test */
+    public function it_accepts_a_forums_flag()
+    {
+
+        $this->artisan('ee-forum:migrate --forums')
+            ->expectsOutput('Migrating Forums')
+            ->assertExitCode(0);
+
+
+        $this->artisan('ee-forum:migrate -f')
+            ->expectsOutput('Migrating Forums')
+            ->assertExitCode(0);
+
+    }
+
+    /** @test */
+    public function it_accepts_a_topics_flag()
+    {
+
+        $this->artisan('ee-forum:migrate --topics')
+            ->expectsOutput('Migrating Forum Topics')
+            ->assertExitCode(0);
+
+
+        $this->artisan('ee-forum:migrate -t')
+            ->expectsOutput('Migrating Forum Topics')
+            ->assertExitCode(0);
+
+    }
+
+    /** @test */
+    public function it_accepts_a_all_flag()
+    {
+
+        $this->artisan('ee-forum:migrate --comments')
+            ->expectsOutput('Migrating Forum Comments')
+            ->assertExitCode(0);
+
+
+        $this->artisan('ee-forum:migrate -c')
+            ->expectsOutput('Migrating Forum Comments')
+            ->assertExitCode(0);
+
+    }
+
+    /** @test */
+    public function it_accepts_an_all_flag()
+    {
+
+        $this->artisan('ee-forum:migrate --all')
+            ->expectsOutput('Migrating All Forum Components')
+            ->assertExitCode(0);
+
+
+        $this->artisan('ee-forum:migrate -a')
+            ->expectsOutput('Migrating All Forum Components')
+            ->assertExitCode(0);
+
     }
 
 }
