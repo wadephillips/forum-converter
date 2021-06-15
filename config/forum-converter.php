2d3dc995cef2e47dbe66074afba8ba362c6afb74
Wade Phillips <phillips.acupuncture@gmail.com>
2021-06-15 14:30:56 -0800

    Add prefix ability to legacy db

--- config/forum-converter.php
+++ config/forum-converter.php
@@ -14,7 +14,7 @@ return [
                 'unix_socket' => env('LEGACY_DB_SOCKET', ''),
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
+                'prefix' => env('LEGACY_DB_PREFIX', ''),
                 'prefix_indexes' => true,
                 'strict' => true,
                 'engine' => null,
