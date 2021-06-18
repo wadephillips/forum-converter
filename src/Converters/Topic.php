02107ae45d4ed315b29db99a7e34926cdc60c5cd
Wade Phillips <phillips.acupuncture@gmail.com>
2021-06-18 06:26:08 -0800

    Setup Converter\Topic

--- src/Converters/Topic.php
+++ src/Converters/Topic.php
@@ -3,6 +3,22 @@
 
 namespace wadelphillips\ForumConverter\Converters;
 
+
+use wadelphillips\ForumConverter\Models\LegacyTopic;
+use wadelphillips\ForumConverter\Models\Topic as TopicPost;
+use function dd;
+
 class Topic
 {
+    public static function migrate(LegacyTopic $legacyTopic, array $options = []): TopicPost
+    {
+        if (! empty($options)) {
+            dd('need to handle the options!');
+        }
+
+
+        $topic = new TopicPost();
+        
+        return $topic;
+    }
 }
