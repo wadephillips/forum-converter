e0da218ab96daba585a098a58768bb0794ab907f
Wade Phillips <phillips.acupuncture@gmail.com>
2021-09-17 12:20:35 -0800

    wip

--- src/Services/QuoteReplacer.php
+++ src/Services/QuoteReplacer.php
@@ -54,7 +54,24 @@ class QuoteReplacer
             $positionClosing = strpos($part, $this->closingTag);
             if ($positionClosing > 0) {
                 //get the attributes
-                sub
+                //todo resume: working on getting this as json in tinkerwell the decode to array
+                //start from tinkerwell
+                $a = '[quote] vivamus feugiat magnis parturient orci sit mauris est[/quote], netus nunc felis per blandit quis suspendisse integer hendrerit, varius duis erat auctor tortor mollis montes tellus. Posuere neque tellus conubia eu cum duis sapien risus, justo sociosqu inceptos nisi nostra magna venenatis. [quote author="obnicole" date="1327507679"] scelerisque convallis diam tristique massa[/quote] urna quam, nibh tempor hendrerit mattis facilisi eleifend vel per adipiscing pellentesque, tincidunt nulla felis donec turpis [strong]curabitur faucibus eget in. [url=Sed vehicula euismod etiam] nisl iaculis neque enim nullam ultrices, vulputate facilisi nec blandit est sagittis nunc suspendisse varius[/url], nibh cursus lobortis [color=red]laoreet id dignissim[/color] posuere sapien. Pellentesque conubia condimentum auctor nec varius suspendisse facilisis elit taciti dictum, eleifend vel nascetur feugiat lorem sit cras ullamcorper. Dui suspendisse auctor rhoncus porta tempus lectus lorem, vestibulum volutpat mi penatibus vel dignissim.
+';
+
+                $converter = new wadelphillips\ForumConverter\Services\QuoteReplacer($a);
+
+// $converter->process()
+                $b = explode('[quote', $a);
+                $pos = strpos($b[2], ']');
+
+// Str:Str::replace('author','"author"',Str::replace( '=', ':', ))
+                $s = Str::of(substr($b[2], 1, $pos-1))->replace('=', ':')->replace('author', '"author"')->replace('date', '"date"')->replace(' ', ', ');
+
+                json_decode(strval($s), true);
+                strval($s);
+// end from tinkerwell
+                substr();
             }
             // cut out the content
 
